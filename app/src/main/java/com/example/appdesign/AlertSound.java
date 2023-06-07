31ef3d73ebf059c50dc3b468fbf62fd78293157a
unknown <sonada903@gmail.com>
2023-06-07 15:26:17 +0900

    bluetooth

---
+++ app/src/main/java/com/example/appdesign/AlertSound.java
@@ -0,0 +1,35 @@
+package com.example.appdesign;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class AlertSound extends Service {
+    MediaPlayer player;
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        player = MediaPlayer.create(this,R.raw.alert);
+        player.setLooping(false);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        player.start();
+        return super.onStartCommand(intent, flags, startId);
+    }
+}
--- app/src/main/java/com/example/appdesign/MainActivity.java
+++ app/src/main/java/com/example/appdesign/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.appdesign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -13,7 +16,44 @@ import android.widget.GridLayout;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.text.BreakIterator;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -21,6 +61,28 @@ public class MainActivity extends AppCompatActivity {
     View view1,view2,view3,view4;
     ImageButton imgbtn1,imgbtn2,imgbtn3,imgbtn4;
     boolean isimgbtn1,isimgbtn2,isimgbtn3,isimgbtn4 = true;
+
+
+    //bluetooth 관련 변수
+    private static final int REQUEST_ENABLE_BT = 1;
+    public BluetoothAdapter mBluetoothAdapter;
+    public Set<BluetoothDevice> mDevices;
+    public boolean onBT = false;
+    public ProgressDialog asyncDialog;
+    private BluetoothSocket bSocket;
+    private OutputStream mOutputStream;
+    private InputStream mInputStream;
+    private BluetoothDevice mRemoteDevice;
+    private Button BTButton;
+
+    private static final byte COMMAND1_BTN_ON = 0x21;
+    private static final byte COMMAND1_BTN_OFF = 0x23;
+    private static final byte COMMAND2_BTN_ON = 0x25;
+    private static final byte COMMAND2_BTN_OFF = 0x27;
+    private static final byte COMMAND3_BTN_ON = 0x29;
+    private static final byte COMMAND3_BTN_OFF = 0x31;
+    private static final byte COMMAND4_BTN_ON = 0x33;
+    private static final byte COMMAND4_BTN_OFF = 0x35;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -188,6 +250,50 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //블루투스 연결 관련부
+        BTButton = findViewById(R.id.connect);
+        BTButton.setOnClickListener(new Button.OnClickListener() {
+            @SuppressLint({"SetTextI18n", "MissingPermission"})
+            @Override
+            public void onClick(View view) {
+                if (!onBT) { //Connect
+                    mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+                    if (mBluetoothAdapter == null) { //장치가 블루투스를 지원하지 않는 경우.
+                        Toast.makeText(getApplicationContext(), "Bluetooth 지원을 하지 않는 기기입니다.", Toast.LENGTH_SHORT).show();
+                    } else { // 장치가 블루투스를 지원하는 경우.
+                        if (!mBluetoothAdapter.isEnabled()) {
+                            // 블루투스를 지원하지만 비활성 상태인 경우
+                            // 블루투스를 활성 상태로 바꾸기 위해 사용자 동의 요청
+                            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                        } else {
+                            // 블루투스를 지원하며 활성 상태인 경우
+                            // 페어링된 기기 목록을 보여주고 연결할 장치를 선택.
+                            Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+                            if (pairedDevices.size() > 0) {
+                                // 페어링 된 장치가 있는 경우.
+                                selectDevice();
+                            } else {
+                                // 페어링 된 장치가 없는 경우.
+                                Toast.makeText(getApplicationContext(), "먼저 Bluetooth 설정에 들어가 페어링을 진행해 주세요.", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                } else { //DisConnect
+                    try {
+                        mInputStream.close();
+                        mOutputStream.close();
+                        bSocket.close();
+                        setButtonEnabled(false);
+                        onBT = false;
+                        BTButton.setText("connect");
+                    } catch (Exception ignored) {
+                    }
+                }
+            }
+        });
+
+
         //이미지 버튼
         imgbtn1 = findViewById(R.id.imgbtn1);
         imgbtn2 = findViewById(R.id.imgbtn2);
@@ -204,9 +310,11 @@ public class MainActivity extends AppCompatActivity {
                 if (isimgbtn1) {
                     imgbtn1.setImageResource(R.drawable.imgbutton);
                     isimgbtn1 = false;
+                    sendBTCommand(COMMAND1_BTN_ON);
                 } else {
                     imgbtn1.setImageResource(R.drawable.imgbtn2);
                     isimgbtn1 = true;
+                    sendBTCommand(COMMAND1_BTN_OFF);
                 }
             }
         });
@@ -264,7 +372,6 @@ public class MainActivity extends AppCompatActivity {
                 imgbtn2.setImageResource(R.drawable.imgbutton);
                 imgbtn3.setImageResource(R.drawable.imgbutton);
                 imgbtn4.setImageResource(R.drawable.imgbutton);
-                Log.d("MyActivity", "allon 버튼이 클릭되었습니다.");
             }
         });
 
@@ -294,5 +401,188 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+    }
+    @SuppressLint("MissingPermission")
+    private void selectDevice() {
+        mDevices = mBluetoothAdapter.getBondedDevices();
+        final int mPairedDeviceCount = mDevices.size();
+
+        if (mPairedDeviceCount == 0) {
+            //  페어링 된 장치가 없는 경우
+            Toast.makeText(getApplicationContext(), "장치를 페어링 해주세요!", Toast.LENGTH_SHORT).show();
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("블루투스 장치 선택");
+
+
+        // 페어링 된 블루투스 장치의 이름 목록 작성
+        List<String> listItems = new ArrayList<>();
+        for (BluetoothDevice device : mDevices) {
+            listItems.add(device.getName());
+        }
+        listItems.add("취소");    // 취소 항목 추가
+
+        final CharSequence[] items = listItems.toArray(new CharSequence[listItems.size()]);
+
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int item) {
+                if (item == mPairedDeviceCount) {
+                    // 연결할 장치를 선택하지 않고 '취소'를 누른 경우
+                    //finish();
+                } else {
+                    // 연결할 장치를 선택한 경우
+                    // 선택한 장치와 연결을 시도함
+                    connectToSelectedDevice(items[item].toString());
+                }
+            }
+        });
+
+        builder.setCancelable(false);    // 뒤로 가기 버튼 사용 금지
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    private void connectToSelectedDevice(final String selectedDeviceName) {
+        mRemoteDevice = getDeviceFromBondedList(selectedDeviceName);
+
+        //Progress Dialog
+        asyncDialog = new ProgressDialog(MainActivity.this);
+        asyncDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        asyncDialog.show();
+        asyncDialog.setCancelable(false);
+
+        Thread BTConnect = new Thread(new Runnable() {
+            @SuppressLint("MissingPermission")
+            public void run() {
+                try {
+                    UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"); //HC-06 UUID
+                    // 소켓 생성
+                    bSocket = mRemoteDevice.createRfcommSocketToServiceRecord(uuid);
+
+                    // RFCOMM 채널을 통한 연결
+                    bSocket.connect();
+
+                    // 데이터 송수신을 위한 스트림 열기
+                    mOutputStream = bSocket.getOutputStream();
+                    mInputStream  = bSocket.getInputStream();
+                    runOnUiThread(new Runnable() {
+
+                        @SuppressLint({"ShowToast", "SetTextI18n"})
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(), selectedDeviceName + " 연결 완료", Toast.LENGTH_LONG).show();
+                            BTButton.setText("disconnect");
+                            setButtonEnabled(true);
+                            asyncDialog.dismiss();
+                            receiveDate();
+
+                        }
+                    });
+                    onBT = true;
+                } catch (Exception e) {
+                    // 블루투스 연결 중 오류 발생
+                    runOnUiThread(new Runnable() {
+                        @SuppressLint({"ShowToast", "SetTextI18n"})
+                        @Override
+                        public void run() {
+                            asyncDialog.dismiss();
+                            Toast.makeText(getApplicationContext(), "블루투스 연결 오류", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        });
+        BTConnect.start();
+    }
+
+    private void setButtonEnabled(boolean enabled) {
+//        btnOn.setEnabled(enabled);
+//        btnOff.setEnabled(enabled);
+        imgbtn1.setEnabled(enabled);
+        imgbtn2.setEnabled(enabled);
+    }
+    @SuppressLint("MissingPermission")
+    public BluetoothDevice getDeviceFromBondedList(String name) {
+        for (BluetoothDevice device : mDevices) {
+            if (name.equals(device.getName())) {
+                return device;
+            }
+        }
+        return null;
+    }
+
+    public void sendBTCommand(byte cmd) {
+        try {
+            mOutputStream.write(new byte[]{(byte) cmd});    // 프로토콜 전송
+            mOutputStream.flush();
+        } catch (Exception e) {
+            // 문자열 전송 도중 오류가 발생한 경우.
+            asyncDialog.setMessage("failed");
+        }
+    }
+
+    public void receiveDate() {
+        final Handler handler = new Handler();
+        boolean _isRunning;
+        // 화재 감지시 토스트 메시지 ..
+        Thread receiveThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (this) {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            int bytesAvailable = mInputStream.available();
+
+                            char read = (char) mInputStream.read();
+                            if (read == 'q') {
+                                LayoutInflater layout2 = getLayoutInflater();
+                                startService(new Intent(MainActivity.this, AlertSound.class));
+//                                View v2 = layout2.inflate(null, null);
+                                MainActivity.this.runOnUiThread(() -> {
+
+                                    Toast.makeText(MainActivity.this, "화재가 감지되었습니다.", Toast.LENGTH_SHORT).show();
+                                    new AlertDialog.Builder(MainActivity.this)
+                                            .setTitle("화재발생")
+                                            .setMessage("119에 연결하시겠습니까?")
+                                            .setPositiveButton("네", new DialogInterface.OnClickListener()
+                                            {
+
+                                                @RequiresApi(api = Build.VERSION_CODES.M)
+                                                @Override
+                                                public void onClick(DialogInterface dialog, int which) {
+                                                    requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, 1000);
+                                                    stopService(new Intent(MainActivity.this, AlertSound.class));
+                                                    Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:01075942362"));
+                                                    startActivity(intent);
+
+                                                }
+                                            })
+                                            .setNegativeButton("아니요", new DialogInterface.OnClickListener() {
+                                                @Override
+                                                public void onClick(DialogInterface dialog, int which) {
+                                                    stopService(new Intent(MainActivity.this, AlertSound.class));
+                                                }
+                                            }).show();
+                                });
+                            }
+                            char a = (char) mInputStream.read();
+                            if (a == 'z') {
+                                stopService(new Intent(MainActivity.this, AlertSound.class));
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        });
+        //데이터 수신 thread 시작
+        receiveThread.start();
     }
 }
+
+
